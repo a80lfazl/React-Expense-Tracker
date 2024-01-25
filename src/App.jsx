233aa980eafe66d44721e343a5afc3f36056e1b2
amiri1387 <amiriabolfazl992@gmail.com>
2024-01-26 01:53:34 +0330

    Initial setup of the project. v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>I'm in the way of being a "front-end React" developer.</h1>
+      <p>I feel i am a "miserable" person :)</p>
+      <p>I don't have any experiment and no creativity.</p>
+      <p>everyone nage on me but i don't want to give up.</p>
+    </>
+  );
+}
+
+export default App
